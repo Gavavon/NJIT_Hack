c0a4e64ca6d9e81b9d79f5e552dafaa0404e9887
Gavavon <44536632+Gavavon@users.noreply.github.com>
2019-11-09 11:37:12 -0500

    the whole project

---
+++ NJIT_Hack/src/njit_hack/NJIT_Hack.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package njit_hack;
+
+import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author Gavav
+ */
+public class NJIT_Hack extends JPanel implements KeyListener {
+
+    final int SCREEN_WIDTH = 600;
+    final int SCREEN_HEIGHT = 700;
+    //delcare here
+    public NJIT_Hack(){
+        //initialize here
+    }
+    public void update(){
+        //update stuff here
+    }
+    public void paint(Graphics g) {
+        super.paint(g);
+        update();
+        //graphics
+        
+        
+        
+        
+        repaint();
+    }
+    public final void print(Object b) {
+        System.out.println(b);
+        //this makes printing easier
+    }
+    public static void main(String[] args) {
+        NJIT_Hack game = new NJIT_Hack();
+        JFrame frame = new JFrame();
+        frame.setTitle("SpaceGame");
+        frame.add(game);
+        frame.pack();
+        frame.setResizable(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    
+    }
+    
+}
