ab5653e79d219a31e39f8ad078fa509b45872983
jkpuzon <jankarlp@gmail.com>
2019-11-09 22:12:48 -0500

    perfect sqaures

--- NJIT_Hack/src/njit_hack/NJIT_Hack.java
+++ NJIT_Hack/src/njit_hack/NJIT_Hack.java
@@ -31,62 +31,65 @@ public class NJIT_Hack extends JPanel implements KeyListener {
 
     final int SCREEN_WIDTH = 1200;
     final int SCREEN_HEIGHT = 650;
-    
+
     Rectangle backDrop;
     Rectangle ground;
     Rectangle playerRect;
     Rectangle playerTimeBar;
     Rectangle buildings[];
-    
+    Rectangle timerBar[];
+
     int buildingX = 0;
-    
+
     Image timeBar;
-    
+
     int amountPotions = 3;
-    
+
     boolean freeze = false;
-    
+
     String text = "";
-    
+
     public Random gen = new Random();
-    
+
     public PlayerStats player = new PlayerStats();
     public EnemeyStats enemey = new EnemeyStats();
     public Sword1 sword1 = new Sword1();
-    
-    
-    public NJIT_Hack(){
+
+    public NJIT_Hack() {
         setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
         setFocusable(true);
         addKeyListener(this);
-        
+
         player.setHealth(100);
         player.setAttack(0);
         player.setSpeed(0);
         player.setDefense(0);
         player.setCriticalChance(0);
-        
-        ground = new Rectangle(0,(SCREEN_HEIGHT /3)*2,SCREEN_WIDTH,(SCREEN_HEIGHT/3)+2);
-        backDrop = new Rectangle(0,0,SCREEN_WIDTH,SCREEN_HEIGHT);
-        playerRect = new Rectangle((int) (SCREEN_WIDTH / (double) (4.8)), (int)(SCREEN_HEIGHT / (double) (2.6)),(SCREEN_WIDTH / 8),(SCREEN_HEIGHT/3) + ((SCREEN_HEIGHT/3) / 8));
-        
-        playerTimeBar = new Rectangle(playerRect.x, playerRect.y - (SCREEN_HEIGHT/(26/3)), playerRect.width, 50);
-        
+
+        ground = new Rectangle(0, (SCREEN_HEIGHT / 3) * 2, SCREEN_WIDTH, (SCREEN_HEIGHT / 3) + 2);
+        backDrop = new Rectangle(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+        playerRect = new Rectangle((int) (SCREEN_WIDTH / (double) (4.8)), (int) (SCREEN_HEIGHT / (double) (2.6)), (SCREEN_WIDTH / 8), (SCREEN_HEIGHT / 3) + ((SCREEN_HEIGHT / 3) / 8));
+
+        playerTimeBar = new Rectangle(playerRect.x, playerRect.y - (SCREEN_HEIGHT / (26 / 3)), playerRect.width + 2, 50);
+
         buildings = new Rectangle[10];
-        
-        for (int i =0; i < buildings.length; i++){
-            buildings[i] = new Rectangle(SCREEN_WIDTH + 200 + gen.nextInt(1000), ground.y - ((SCREEN_HEIGHT/8) + (i * 25)), (SCREEN_WIDTH/12), (SCREEN_HEIGHT/8) + (i * 25));
+
+        for (int i = 0; i < buildings.length; i++) {
+            buildings[i] = new Rectangle(SCREEN_WIDTH + 200 + gen.nextInt(1000), ground.y - ((SCREEN_HEIGHT / 8) + (i * 25)), (SCREEN_WIDTH / 12), (SCREEN_HEIGHT / 8) + (i * 25));
         }
-        
-        try{
-            
+        timerBar = new Rectangle[10];
+        for (int i = 0; i < timerBar.length; i++) {
+            timerBar[i] = new Rectangle(playerTimeBar.x + 8 + (i * 14), playerTimeBar.y + 5, 10, 40);
+
+        }
+        try {
+
             timeBar = ImageIO.read(new File("src/njit_hack/Time Scale.png"));
-           
 
         } catch (IOException e) {
 
         }
-        
+
         /*
         Rogue class:
         health = 100;
@@ -105,43 +108,43 @@ public class NJIT_Hack extends JPanel implements KeyListener {
         speed = 0
         defense = 0
         critical = 100
-        */
-      
+         */
     }
-    public void update(){
+
+    public void update() {
         //update stuff here
-        for(int i = 0; i < buildings.length; i ++){
+        for (int i = 0; i < buildings.length; i++) {
             freezeBuilding(buildings[i], buildings[i].x, freeze);
         }
-        
-        
-        
-        
+
     }
+
     public void paint(Graphics g) {
         super.paint(g);
         update();
         g.setColor(Color.BLACK);
-        g.fillRect(backDrop.x,backDrop.y,backDrop.width,backDrop.height);
+        g.fillRect(backDrop.x, backDrop.y, backDrop.width, backDrop.height);
         g.setColor(Color.RED);
-        for(int i = 0; i < buildings.length; i ++){
+        for (int i = 0; i < buildings.length; i++) {
             g.fillRect(buildings[i].x, buildings[i].y, buildings[i].width, buildings[i].height);
-        
+
         }
-        
-        
+
         g.setColor(Color.GRAY);
         g.fillRect(ground.x, ground.y, ground.width, ground.height);
         g.setColor(Color.GREEN);
-        g.fillRect(playerRect.x, playerRect.y,playerRect.width , playerRect.height);
-        
+        g.fillRect(playerRect.x, playerRect.y, playerRect.width, playerRect.height);
+
         g.drawImage(timeBar, playerTimeBar.x, playerTimeBar.y, playerTimeBar.width, playerTimeBar.height, null);
-        
-        
-        g.drawString("" + text, 50 , 50);
-        
-        repaint();
-    }
+
+        g.drawString("" + text, 50, 50);
+        for (int i = 0; i < timerBar.length; i++) {
+            g.fillRect(timerBar[i].x, timerBar[i].y, timerBar[i].width, timerBar[i].height);
+        }
+            repaint();
+        }
+    
+
     public void buildMove(Rectangle build, int x) {
         build.x -= 1;
         if (build.x == -200) {
@@ -149,89 +152,100 @@ public class NJIT_Hack extends JPanel implements KeyListener {
         }
 
     }
-    public void freezeBuilding(Rectangle build, int x, boolean freeze){
+
+    public void freezeBuilding(Rectangle build, int x, boolean freeze) {
         if (!freeze) {
             buildMove(build, x);
         }
-        
+
     }
-    public void playerAttack(){
+
+    public void playerAttack() {
         int multiplier = 1;
-        
-        if(criticalHit(player.getCriticalChance())){
+
+        if (criticalHit(player.getCriticalChance())) {
             multiplier = 2;
         }
-        
+
         int enemeyHealth = enemey.getHealth();
-        
+
         enemeyHealth -= player.getAttack();
-        
+
     }
-    public void playerEvade(){
-        
+
+    public void playerEvade() {
+
     }
-    public void playerDefend(){
-        
+
+    public void playerDefend() {
+
     }
-    public void playerUsePotion(){
-        if(amountPotions > 0){
+
+    public void playerUsePotion() {
+        if (amountPotions > 0) {
             amountPotions -= 1;
             player.setHealth(player.getHealth() + 50);
-            if(player.getHealth() > 100){
+            if (player.getHealth() > 100) {
                 int temp = 100 - player.getHealth();
                 player.setHealth(player.getHealth() - temp);
             }
         }
-        
+
     }
-    
-    public void enemeyAttack(){
+
+    public void enemeyAttack() {
         int multiplier = 1;
-        
-        if(criticalHit(player.getCriticalChance())){
+
+        if (criticalHit(player.getCriticalChance())) {
             multiplier = 2;
         }
-        
+
         int enemeyHealth = enemey.getHealth();
         Object temp = isEquipped();
         //enemeyHealth  -= player.getAttack() + temp.get_Atk();
     }
-    public void enemeyEvade(){
-        
+
+    public void enemeyEvade() {
+
     }
-    public void enemeyDefend(){
-        
+
+    public void enemeyDefend() {
+
     }
-    public void enemeyUsePotion(){
-        if(amountPotions > 0){
+
+    public void enemeyUsePotion() {
+        if (amountPotions > 0) {
             amountPotions -= 1;
             player.setHealth(player.getHealth() + 50);
-            if(player.getHealth() > 100){
+            if (player.getHealth() > 100) {
                 int temp = 100 - player.getHealth();
                 player.setHealth(player.getHealth() - temp);
             }
         }
-        
+
     }
-    
-    public Object isEquipped(){
-        if(sword1.equipped){
+
+    public Object isEquipped() {
+        if (sword1.equipped) {
             return sword1;
         }
-        
+
         return null;
     }
-    public boolean criticalHit(int cChance){
-        int temp = gen.nextInt(100)+1;
-        if(cChance <= temp){
+
+    public boolean criticalHit(int cChance) {
+        int temp = gen.nextInt(100) + 1;
+        if (cChance <= temp) {
             return true;
         }
         return false;
     }
+
     public final void print(Object b) {
         System.out.println(b);
         //this makes printing easier
     }
+
     public static void main(String[] args) {
         NJIT_Hack game = new NJIT_Hack();
         JFrame frame = new JFrame();
@@ -247,13 +261,13 @@ public class NJIT_Hack extends JPanel implements KeyListener {
     @Override
     public void keyTyped(KeyEvent e) {
         //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    
+
     }
 
     @Override
     public void keyPressed(KeyEvent e) {
         //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-        
+
     }
 
     @Override
@@ -272,10 +286,10 @@ public class NJIT_Hack extends JPanel implements KeyListener {
             playerDefend();
         }
         if (e.getKeyCode() == KeyEvent.VK_T) {
-            
+
         }
         if (e.getKeyCode() == KeyEvent.VK_Y) {
-            
+
         }
         if (e.getKeyCode() == KeyEvent.VK_Z) {
             //yes
@@ -287,5 +301,5 @@ public class NJIT_Hack extends JPanel implements KeyListener {
             System.exit(0);
         }
     }
-    
+
 }
