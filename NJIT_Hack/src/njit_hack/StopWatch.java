00cb0c0e987467a7416458d9c81f4e0325a90e7e
Gavavon <44536632+Gavavon@users.noreply.github.com>
2019-11-10 04:03:19 -0500

    nfew

--- NJIT_Hack/src/njit_hack/StopWatch.java
+++ NJIT_Hack/src/njit_hack/StopWatch.java
@@ -13,8 +13,9 @@ public class StopWatch extends NJIT_Hack {
 
     private long Start = 0;
     private long End = 0;
-    long elapsed = 0;
+    private long elapsed = 0;
     private boolean run = false;
+    private long end = 0;
     
 
     public void start(){
@@ -22,18 +23,12 @@ public class StopWatch extends NJIT_Hack {
         this.run = true;
     }
     
-       public void end(){
-        this.End = kingCrimson - System.currentTimeMillis();
-        this.run = true;
-    }
-
+    
     public long getStart() {
         return Start;
     }
 
-    public long getEnd() {
-        return End;
-    }
+ 
 
     public boolean isRun() {
         return run;
@@ -43,17 +38,16 @@ public class StopWatch extends NJIT_Hack {
         this.Start = Start;
     }
 
-    public void setEnd(long End) {
-        this.End = End;
-    }
-
     public void setRun(boolean run) {
         this.run = run;
     }
 
     public long getElapsed() { 
-        elapsed = Start - End;
+        end = System.currentTimeMillis()- kingCrimson;
+        elapsed = end - Start;
         return elapsed;
+        pokemon;
     }
-    
+    //i am legend
 }
+
